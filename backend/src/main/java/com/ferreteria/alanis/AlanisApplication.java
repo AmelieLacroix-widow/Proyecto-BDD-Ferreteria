ff9f8b0cc898475843b1f93aa8f0630337c205a1
Amelie Lacroix <lacroix.amelie.widow@gmail.com>
2026-04-01 18:22:57 -0600

    Agregando parte de backend con springboot

---
+++ backend/src/main/java/com/ferreteria/alanis/AlanisApplication.java
@@ -0,0 +1,13 @@
+package com.ferreteria.alanis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlanisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlanisApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/ferreteria/alanis/AlanisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ferreteria.alanis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlanisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
