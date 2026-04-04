292e0e39f2f95882fb4a02f28817de52b5d69a2f
TadeoCB <tadeobazaldua@gmail.com>
2026-04-03 18:31:47 -0600

    Agregado modelo de usuario

---
+++ backend/src/main/java/com/miempresa/ferreteria/model/Usuario.java
@@ -0,0 +1,45 @@
+package com.miempresa.ferreteria.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "usuario")
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_usuario")
+    private Integer idUsuario; // PK 
+
+    @Column(name = "nombre_usuario", length = 100)
+    private String nombreUsuario; // 
+
+    @Column(name = "contrasena_hash", length = 255)
+    private String contrasenaHash; // 
+
+    // --- Getters y Setters ---
+
+    public Integer getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(Integer idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public String getNombreUsuario() {
+        return nombreUsuario;
+    }
+
+    public void setNombreUsuario(String nombreUsuario) {
+        this.nombreUsuario = nombreUsuario;
+    }
+
+    public String getContrasenaHash() {
+        return contrasenaHash;
+    }
+
+    public void setContrasenaHash(String contrasenaHash) {
+        this.contrasenaHash = contrasenaHash;
+    }
+}
