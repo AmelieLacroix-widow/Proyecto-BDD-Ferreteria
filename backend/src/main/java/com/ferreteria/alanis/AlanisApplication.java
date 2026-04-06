259e07dbbad85a2f6864f7ec7d279a0f92706dfa
Amelie Lacroix <lacroix.amelie.widow@gmail.com>
2026-04-05 20:35:25 -0600

    .

--- backend/src/main/java/com/ferreteria/alanis/AlanisApplication.java
+++ backend/src/main/java/com/ferreteria/alanis/AlanisApplication.java
@@ -2,11 +2,15 @@ package com.ferreteria.alanis;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(scanBasePackages = {
     "com.ferreteria.alanis",
     "com.miempresa.ferreteria"
 })
+
+@EnableJpaRepositories(basePackages = "com.miempresa.ferreteria.repository")
+
 public class AlanisApplication {
 
     public static void main(String[] args) {
