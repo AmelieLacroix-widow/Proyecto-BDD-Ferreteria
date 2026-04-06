484601cdf445ed4345eee95b872aaeec12191b0c
Amelie Lacroix <lacroix.amelie.widow@gmail.com>
2026-04-05 20:30:39 -0600

    .

--- backend/src/main/java/com/ferreteria/alanis/AlanisApplication.java
+++ backend/src/main/java/com/ferreteria/alanis/AlanisApplication.java
@@ -3,11 +3,14 @@ package com.ferreteria.alanis;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {
+    "com.ferreteria.alanis",
+    "com.miempresa.ferreteria"
+})
 public class AlanisApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AlanisApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AlanisApplication.class, args);
+    }
 
 }
